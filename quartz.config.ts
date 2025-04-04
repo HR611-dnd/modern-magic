958275a7bbaf74e62ac8b099103efe2c70ccb482
Hayden Ruiz <haydenr1111@gmail.com>
2025-04-03 23:18:40 -0700

    Quartz sync: Apr 3, 2025, 11:18 PM

--- quartz.config.ts
+++ quartz.config.ts
@@ -8,7 +8,7 @@ import * as Plugin from "./quartz/plugins"
  */
 const config: QuartzConfig = {
   configuration: {
-    pageTitle: "Quartz 4",
+    pageTitle: "Modern Magic",
     pageTitleSuffix: "",
     enableSPA: true,
     enablePopovers: true,
